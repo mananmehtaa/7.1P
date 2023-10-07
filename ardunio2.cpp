5e310e3b4df1a64530ef4203aad93381dc5a99fd
mananmehtaa <106482879+mananmehtaa@users.noreply.github.com>
2023-10-07 21:33:23 +1100

    Add files via upload

--- ardunio2.cpp
+++ ardunio2.cpp
@@ -1,20 +1,29 @@
-#include <LiquidCrystal_I2C.h>
+const int photodiodePin = A0; // Analog pin for photodiode
+const int relayPin = 2;       // Digital pin to control relay
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
+int lightThreshold = 500; // Adjust this value based on ambient light conditions
+int detectionRange = 200; // Define the range within which a person is detected
 
 void setup()
 {
-    lcd.begin(16, 2);   // Initialize LCD
-    Serial.begin(9600); // Start serial communication
+  pinMode(photodiodePin, INPUT);
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW); // Initially, turn off the relay
 }
 
 void loop()
 {
-    if (Serial.available() > 0)
-    {
-        String message = Serial.readStringUntil('\n');
-        lcd.clear();        // Clear the LCD
-        lcd.print(message); // Print the received message
-    }
-    delay(500); // Delay for readability
+  int lightIntensity = analogRead(photodiodePin);
+
+  if (lightIntensity < lightThreshold)
+  {
+    // Person detected within the range, turn on the light
+    digitalWrite(relayPin, HIGH);
+    delay(1000); // Wait for a second to stabilize the detection
+  }
+  else
+  {
+    // No person detected, turn off the light
+    digitalWrite(relayPin, LOW);
+  }
 }
